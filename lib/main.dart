180a34e0bcf791a1c42fb704acaa17911338c436
ayman elkafrawy <120125370+mailaymanis@users.noreply.github.com>
2025-03-14 02:36:32 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SupabaseApp());
+}
+
+class SupabaseApp extends StatelessWidget {
+  const SupabaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner:false,
+    );
+  }
+}
+
+
+
+
